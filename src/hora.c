5bfc00cd346ec23fe34bdb774070199572a388d9
DnRT <kn_ct_5290@hotmail.com>
2020-10-14 10:57:59 -0300

    pid listo

--- src/hora.c
+++ src/hora.c
@@ -1,7 +1,11 @@
-#include <stdio>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdlib.h>
 int main(){
-    int test=getpid();
-    printf(test);
-    return EXIT_SUCCESS;
+    pid_t test=getpid();
+    printf("Programa hora ejecutandose. PIR:%d\n", test);
+    printf("Listo para recibir la señal SIGUSR1.\n");
+    
+    return 0;
 }
